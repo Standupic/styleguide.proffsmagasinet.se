48456c8edb954ee368ff49fee607329cdc7b5905
Mikhail Smal <mvsmal@gmail.com>
2020-01-08 14:12:55 +0100

    Add autoprefixer

--- src/AppButton.stories.fs
+++ src/AppButton.stories.fs
@@ -1,10 +1,11 @@
 module AppButton
 
 open Fable.React
+open Fable.React.Props
 open PM.StyleGuide.Storybook
 
 let MyButton =
-    FunctionComponent.Of (fun () -> div [] [ str "Hi!" ])
+    FunctionComponent.Of (fun () -> div [ Class "display-flex" ] [ str "Hi!" ])
     
 storiesOf("MyButton").add("MyButton", fun _ -> MyButton()) |> ignore
 
