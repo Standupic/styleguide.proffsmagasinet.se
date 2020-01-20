2bbefdc58044e18ad2a86aa1edb827ad86067beb
standupic <satndupic87@gmail.com>
2020-01-20 18:34:50 +0300

    Create DropDownMenu

--- src/Verktygsproffsen/HeaderDropDownMenu.stories.fs
+++ src/Verktygsproffsen/HeaderDropDownMenu.stories.fs
@@ -11,35 +11,88 @@ type Category =
         Children : Category list
     }
 
+type ListItemProps =
+    {
+        name : string
+        url : string
+        children : ReactElement list
+    }
 
-let getString = function
-    | false ->
-        "Close"
-    | true ->
-        "Open"
+let ListItems =
+    FunctionComponent.Of(fun (props: ListItemProps ) ->
 
-let SubMenu =
-    FunctionComponent.Of(fun (props: {|initOpen : bool; name : string; url : string|}) ->
+        let state = Hooks.useState(false)
 
-        let state = Hooks.useState(props.initOpen)
-
-        li [ classList ["open", true;  "close", false]
-             OnMouseEnter (fun _ -> state.update(fun s -> true))
-             OnMouseLeave (fun _ -> state.update(fun s -> false))]
-             [ str (getString state.current)
+        li [   OnMouseEnter (fun _ -> state.update(fun s -> true))
+               OnMouseLeave (fun _ -> state.update(fun s -> false))]
+             [
                a [ Href props.url ] [ str props.name ]
+               if not props.children.IsEmpty && state.current then ul [ Class "open"] props.children
              ]
-
     )
 
 
-let rec toCategoriesHtml (categories : Category list, submenu : ReactElementType) =
-    categories |> List.map (fun c ->
-        let catLi = li [ Class "cat "]
-        let catA = a [ Href c.Url ] [ str c.Name ]
+
+let rec toCategoriesHtml (categories : Category list) =
+    categories
+    |> List.map (fun c ->
+        let listItemProps = {name = c.Name; url = c.Url; children = []}
         match c.Children with
-        | [] -> catLi [ catA ]
-        | children -> submenu {|initOpen = false; name = c.name; url = c.url|} [ul [][toCategoriesHtml(children)]]
+        | [] -> ListItems listItemProps
+        | children ->
+             let childrenLI = toCategoriesHtml(children)
+             ListItems { listItemProps with children = childrenLI }
+    )
+
+
+let DropDownMenu =
+    FunctionComponent.Of(fun (props: Category list) ->
+        ul [] (props |> toCategoriesHtml)
+    )
+
+
+let categories = [
+     {Name = "Car"; Url = "www"; Children = [{ Name = "Audi"; Url = "www";
+                                              Children = [
+                                                          { Name = "A1"; Url = "www"; Children = []}
+                                                          { Name = "A4"; Url = "www"; Children = []}
+                                                          { Name = "A6"; Url = "www"; Children = []}
+                                                          { Name = "A8"; Url = "www"; Children = []}
+                                                         ]
+                                             }
+                                             { Name = "BMW"; Url = "www";
+                                              Children = [
+                                                         { Name = "X1"; Url = "www";
+                                                         Children = [
+                                                                      { Name = "2001"; Url = "www"; Children = []}
+                                                                      { Name = "2002"; Url = "www"; Children = []}
+                                                                      { Name = "2003"; Url = "www"; Children = []}
+                                                                      { Name = "2005"; Url = "www"; Children = []}
+                                                         ]}
+                                                         { Name = "X3"; Url = "www"; Children = []}
+                                                         { Name = "X5"; Url = "www"; Children = []}
+                                                         { Name = "X6"; Url = "www"; Children = []}
+                                                        ]
+                                             }
+                                             ]}
+     {Name = "Moto"; Url = "www"; Children = [{ Name = "Honda"; Url = "www";
+                                                Children = [
+                                                           { Name = "XR"; Url = "www"; Children = []}
+                                                           { Name = "R"; Url = "www"; Children = []}
+                                                           { Name = "Ninja"; Url = "www"; Children = []}
+                                                           { Name = "RS"; Url = "www"; Children = []}
+                                                         ]
+                                             };
+                                             { Name = "BMW"; Url = "www";
+                                              Children = [
+                                                         { Name = "X1"; Url = "www"; Children = []}
+                                                         { Name = "X3"; Url = "www"; Children = []}
+                                                         { Name = "X5"; Url = "www"; Children = []}
+                                                         { Name = "X6"; Url = "www"; Children = []}
+                                                        ]
+                                             }
+                                             ]}
+]
 
 storiesOf("Verktygsproffsen|Header").add("DropDownMenu", fun _ ->
-        SubMenu {|initOpen = false|} )|> ignore
+        DropDownMenu categories )|> ignore
