829323654b957e68d6cdc5864b42aaf7982dc86a
Debashish Halder <debashish110215@gmail.com>
2020-01-21 12:23:35 +0600

    Refactor the code using account type as common type and map with types

--- src/Verktygsproffsen/Home/MarketingBanner.stories.fs
+++ src/Verktygsproffsen/Home/MarketingBanner.stories.fs
@@ -5,19 +5,12 @@ open PM.StyleGuide
 open Fable.React
 open Fable.React.Props
 open Storybook
+open CommonTypes
 
 type BannerType =
     | Verktyg_Premium_2x2
     | Verktyg_Premium_1x2
     | Verktyg_Premium_1x1
-
-type BannerBlockType =
-    | Verktyg_Premium_Organization_2x2
-    | Verktyg_Premium_Organization_1x2
-    | Verktyg_Premium_Organization_1x1
-    | Verktyg_Premium_Customer_2x2
-    | Verktyg_Premium_Customer_1x2
-    | Verktyg_Premium_Customer_1x1
 
 type MarketingBanner =
     {
@@ -26,19 +19,15 @@ type MarketingBanner =
         Image : string
     }
 
-type AccessType =
-    | Customer
-    | Organization
-
 type MarketingBannersProps =
     {
-        AccessType: AccessType
-        Blocks : Map<BannerBlockType, MarketingBanner list>
+        AccountType: AccountType
+        Blocks : Map<AccountType, MarketingBanner list>
     }
 
 let blockList =
     [
-        (Verktyg_Premium_Customer_1x1,
+        (B2C,
             [
                 {
                     MarketingBanner.BannerType = Verktyg_Premium_1x1
@@ -55,19 +44,11 @@ let blockList =
                     Link = "#"
                     Image = "https://pm-image.azureedge.net/dynamic-content/2019_bf/VP_festool_772x460.png"
                 }
-            ]
-        )
-        (Verktyg_Premium_Customer_1x2,
-            [
                 {
                     MarketingBanner.BannerType = Verktyg_Premium_1x2
                     Link = "#"
                     Image = "https://pm-image.azureedge.net/dynamic-content/2019_bf/VP_banner2_772x860.png"
                 }
-            ]
-        )
-        (Verktyg_Premium_Customer_2x2,
-            [
                 {
                     MarketingBanner.BannerType = Verktyg_Premium_2x2
                     Link = "#"
@@ -75,7 +56,7 @@ let blockList =
                 }
             ]
         )
-        (Verktyg_Premium_Organization_1x1,
+        (B2B,
             [
                 {
                     MarketingBanner.BannerType = Verktyg_Premium_1x1
@@ -92,19 +73,11 @@ let blockList =
                     Link = "https://www.verktygsproffsen.se/black-friday?keyword=festool"
                     Image = "https://pm-image.azureedge.net/dynamic-content/2019_bf/VP_festool_772x460.png"
                 }
-            ]
-        )
-        (Verktyg_Premium_Organization_1x2,
-            [
                 {
                     MarketingBanner.BannerType = Verktyg_Premium_1x2
                     Link = "https://www.verktygsproffsen.se/best-offers-black-friday"
                     Image = "https://pm-image.azureedge.net/dynamic-content/2019_bf/VP_banner2_772x860.png"
                 }
-            ]
-        )
-        (Verktyg_Premium_Organization_2x2,
-            [
                 {
                     MarketingBanner.BannerType = Verktyg_Premium_2x2
                     Link = "https://www.verktygsproffsen.se/black-friday"
@@ -114,7 +87,7 @@ let blockList =
         )
     ]
 
-let bannerLink banner =
+let bannerLinkElement banner =
     a [ classList [ ("banner", true)
                     ("banner-main", banner.BannerType = Verktyg_Premium_2x2)
                   ]
@@ -122,43 +95,33 @@ let bannerLink banner =
       ]
       [ img [ Src banner.Image ] ]
 
-let banners banners =
-    div [ Class "banner-list" ] (banners |> List.map bannerLink)
+let bannerElement banners =
+    div [ Class "banner-list" ] (banners |> List.map bannerLinkElement)
 
-let premiumBlockTypes =
-    (Verktyg_Premium_Customer_2x2, Verktyg_Premium_Organization_2x2)
+let blocks allBlocks accountType =
+    allBlocks |> Map.find accountType
 
-let secondPremiumBlockTypes =
-    (Verktyg_Premium_Customer_1x2, Verktyg_Premium_Organization_1x2)
-
-let normalBlockTypes =
-    (Verktyg_Premium_Customer_1x1, Verktyg_Premium_Organization_1x1)
-
-let blockType accessType (customerBlock, organizationBlock) =
-    match accessType, (customerBlock, organizationBlock) with
-    | Customer, (a,_) -> a
-    | _, (_,b) -> b
-
-let blocks allBlocks accessType (customerBlock, organizationBlock) =
-    allBlocks |> Map.find (blockType accessType (customerBlock,organizationBlock))
+let banners blocks bannerType =
+    blocks |> List.filter (fun a -> a.BannerType = bannerType)
 
 let props =
     {
         MarketingBannersProps.Blocks = blockList |> Map.ofList
-        AccessType = Customer
+        AccountType = B2C
     }
 
 let MarketingBanners =
     FunctionComponent.Of(fun (props : MarketingBannersProps) ->
 
-        let mainBanner = blocks props.Blocks props.AccessType premiumBlockTypes
-        let UpperRowBanners = mainBanner @ (blocks props.Blocks props.AccessType secondPremiumBlockTypes)
-        let LowerRowBanners = blocks props.Blocks props.AccessType normalBlockTypes
+        let bannerList = blocks props.Blocks props.AccountType
+        let mainBanner = banners bannerList Verktyg_Premium_2x2
+        let UpperRowBanners = mainBanner @ banners bannerList Verktyg_Premium_1x2
+        let LowerRowBanners = banners bannerList Verktyg_Premium_1x1
 
         div [ Class "marketing-banners" ]
             [
-                banners UpperRowBanners
-                banners LowerRowBanners
+                bannerElement UpperRowBanners
+                bannerElement LowerRowBanners
             ]
     )
 
